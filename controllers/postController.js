5b2c96e9973ebc506b6452c27dc5ba8f8090f053
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-12 19:35:30 +0200

    fix: correct error handling in show and destroy functions for better response management

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,8 @@
-const posts = require("../data/posts");
 const connection = require("../db/connection")
 
 function index(req, res) {
-  const sql = 'SELECT * FROM posts'
-  connection.query(sql, (err, results)=> {
+  const sqlIndex = 'SELECT * FROM posts'
+  connection.query(sqlIndex, (err, results)=> {
     if(err) return res.status(500).json({error: true, message: err.message})
     console.log(err)
     console.log(results)
@@ -14,8 +13,8 @@ function index(req, res) {
 
 function show(req, res) {
   const id = parseInt(req.params.id);
-  const sql = 'SELECT * FROM posts WHERE id = ?'
-  connection.query(sql, [id], (err, results)=> {
+  const sqlSinglePost = 'SELECT * FROM posts WHERE id = ?'
+  connection.query(sqlSinglePost, [id], (err, results)=> {
     if (err) return res.status(500).json({error: 'Database Query Failed'})
       if (!results.length > 0) return res.status(404).json({error: 'Post not found'})
         res.json(results[0])
@@ -26,8 +25,8 @@ function show(req, res) {
 function store(req, res) {
   console.log('This is the req.body')
   const {title, content, image} = req.body
-  const sql = 'INSERT INTO posts (title, content, image) VALUES (?, ?, ?);'
-  connection.query(sql, [title, content, image], (err, results)=> {
+  const sqlNewPost = 'INSERT INTO posts (title, content, image) VALUES (?, ?, ?);'
+  connection.query(sqlNewPost, [title, content, image], (err, results)=> {
     if (err) return res.status(500).json({error: 'Database Query Failed'})
       console.log(results)
     res.status(201).json({id: results.insertId});
@@ -37,23 +36,23 @@ function store(req, res) {
 function update(req, res) {
   const id = parseInt(req.params.id);
   const {title, content, image} = req.body
-  const sql = 'UPDATE posts SET title = ?, content = ?, image = ? WHERE id = ?'
-  connection.query(sql, [title, content, image, id], (err, results)=> {
+  const sqlUpdatePost = 'UPDATE posts SET title = ?, content = ?, image = ? WHERE id = ?'
+  connection.query(sqlUpdatePost, [title, content, image, id], (err, results)=> {
     if (results.affectedRows === 0) {
       return res.status(404).json({
         error:true,
         message: 'Not Found'
       })
     }
-    res.json({ message: 'Post update successfully'});
+    return res.json(results[0])
   })
 }
 
 
 function destroy(req, res) {
   const id = parseInt(req.params.id);
-  const sql = 'DELETE FROM posts WHERE id = ?'
-  connection.query(sql, [id], (err, results) =>{
+  const sqlDestroyPost = 'DELETE FROM posts WHERE id = ?'
+  connection.query(sqlDestroyPost, [id], (err, results) =>{
     if (err) return res.status(500).json({error:'Failed to delete post'})
       console.log(results)
       res.sendStatus(204) 
